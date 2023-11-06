9a14941c993362861b0ab2ac514a08c0fb918d1b
kennymartinez19k <kennymartinez19k@gmail.com>
2023-11-06 15:23:37 -0400

    change homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonModal) modal: any 
 
   constructor() {}
+  message: any = null
+  name = null
+  hourIn = null
+  hourOut = null
+  isModalOpen: any = false
+  cancel() {
+    this.isModalOpen = false
+  }
+  users = [1,2,3]
+
+  confirm() {
+    this.isModalOpen = false
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
+
+  setOpen(condition: Boolean = false){
+    this.isModalOpen = condition;
+  }
 
 }
